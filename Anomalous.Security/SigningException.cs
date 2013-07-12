ac90efcf8918eb09bc8c4684c182a97766406bf5
AndrewPiper <AndrewPiper@localhost>
2013-07-12 16:03:22 +0000

    Made SigningException public.

--- Anomalous.Security/SigningException.cs
+++ Anomalous.Security/SigningException.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Anomalous.Security
 {
-    class SigningException : Exception
+    public class SigningException : Exception
     {
         public SigningException(String message)
             :base(message)
